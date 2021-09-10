76f8a4d82029355629d7f90b5aad071435560d7a
Cynthia <caguillon.techie@gmail.com>
2021-09-10 16:32:15 -0500

    POJO and Feeding Data to Thymeleaf Templates

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 /**
  * Methods in this class should be related to the object the controller
@@ -21,9 +25,10 @@ public class GifController {
     }
 
     @RequestMapping(value = "/gif")
-    @ResponseBody
-    public String gifURI(){
-        return "Captured URI/gif!";
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Peter Parker", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 
 }
